7a7c414f36eaec345fe716deb3079b8bd42e1360
Somondas <somondas067@gmail.com>
2024-03-09 23:35:12 +0530

    First commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+//todo  Use alt + z to enable line break in VS code
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flexContainer   padding-container flexContainer relative z-30 py-5">
+      Navbar lorem21
+    </nav>
+  );
+};
+
+export default Navbar;
