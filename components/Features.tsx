78fccdf1be5c18e82218e3dc44e9e842af9d1ba8
Somondas <somondas067@gmail.com>
2024-03-18 21:09:51 +0530

    Guide Section done Working on Features Section

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <Image
+          src="/phone.png"
+          alt="phone"
+          width={440}
+          height={1000}
+          className="feature-phone"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Features;
